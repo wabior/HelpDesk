dcb4616e36f0b2a92a4d4f04c9d58626f723a943
wabior <wabior@gmail.com>
2020-12-30 07:25:05 +0100

    update indexUser view
    add message if no tickest jet
    add labels for columns

--- resources/views/tickets/indexUser.blade.php
+++ resources/views/tickets/indexUser.blade.php
@@ -2,18 +2,27 @@
 
 @section('content')
 
-    <div class="row">
-
-        <div class="col ml-2 ml-md-4">
+        <div class=" ml-2 ml-md-4 p-3">
             Zgłoszenia użytkownika: {{ Auth::user()->name}} {{ Auth::user()->surname }}
         </div>
 
-    </div>
+
+    @if (!empty($tickets[0]))
+
+            {{-- labels for columns --}}
+        <div class="row mt-1 mb-0 ml-1 d-none d-xl-flex">
+
+            <label class="col-2 col-xl-1 px-0 px-md-3  text-center"> Id </label>
+            <label class="col-10 col-md-4 col-xl-6">Tytuł</label>
+            <label class="col-6 col-md-3 col-xl-2 ml-md-3">Utworzono</label>
+            <label class="col-6 col-md-3 col-xl-2">Status</label>
+        </div>
+
+
 
     @foreach ($tickets as $ticket)
 
-
-    <div class="row mt-1 mb-2 ml-1">
+    <div class="row mt-0 mb-0 ml-1" name="id">
 
                 {{-- id --}}
         <div class="list-group col-2 col-xl-1 px-0  px-md-3">
@@ -50,7 +59,7 @@
             </p>
 
         </div>
-
+                {{-- status --}}
             <div class="list-group col-6 col-md-3 col-xl-2 my-0  px-md-3">
 
                 <p class="list-group-item text-center p-2
@@ -63,6 +72,12 @@
     </div>
 
     @endforeach
+
+    @else
+
+    <div class="bg-info ml-2 ml-md-4 p-3">Nie masz żadnych zgłoszeń.</div>
+
+    @endif
 
 @endsection
 
